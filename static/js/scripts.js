217d904c8049df46dc63052997a4045ce0cb73e1
Florencia <flo.mirandai@gmail.com>
2019-10-16 16:29:27 -0300

    Mis cursos muestra los cursos asociados a docentes.

---
+++ static/js/scripts.js
@@ -0,0 +1,14 @@
+//menú aparece por el lado cuando se achica.
+document.addEventListener('DOMContentLoaded', function() {
+            var elems = document.querySelectorAll('.sidenav');
+            var instances = M.Sidenav.init(elems);
+        });
+
+//acordiones
+ document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems);
+
+  });
+
+
