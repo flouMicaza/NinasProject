c77aeade52ec94c95f026159587808ff037f51fb
fermacias <fernanda.macias@ug.uchile.cl>
2020-01-14 15:43:11 -0300

    clase asistencia modificada

--- asistencia/models.py
+++ asistencia/models.py
@@ -8,17 +8,12 @@ class Asistencia(models.Model):
     alumna = models.ForeignKey('usuarios.User', related_name='alumna' ,on_delete=models.CASCADE)
     clase = models.ForeignKey('clases.Clase', on_delete=models.CASCADE)
     author = models.ForeignKey('usuarios.User', related_name='profesora' ,on_delete=models.CASCADE)  # quien paso la asistencia
+    asistio = models.BooleanField('alumna asisitio')
 
     def __str__(self):
         return "{0} asistió a la clase {1}".format(self.alumna.username, self.clase.nombre)
 
 
-"""
-    def get_asistencia_data(self, **kwargs):
-        context = super(UserPreferences, self).get_context_data(**kwargs)
-        context['form1'] = Preferences.objects.all()
-        return context
-"""
 
 class Asistentes(models.Model):
     asistentes = models.ManyToManyField(User, related_name="asistentes", blank=True)
@@ -28,3 +23,4 @@ class Asistentes(models.Model):
     def guardarAsistencia(self):
         for asistente in self.asistentes:
             Asistencia.objects.create(alumna=asistente, clase=self.clase, author=self.author)
+
