a9991e4a5b9b9591dd0a59584dd38e6a3cc96b78
antolabarca <anto.labarca.s@gmail.com>
2020-01-20 13:08:18 -0300

    cambios en el modelo de las clases (unique) y avance en el get_form

--- asistencia/forms.py
+++ asistencia/forms.py
@@ -6,7 +6,13 @@ from django.forms import formset_factory, modelformset_factory
 
 
 class AsistenciaForm(forms.Form):
-    asistio=forms.BooleanField(label='hola', widget=forms.CheckboxInput())
+    asistio=forms.BooleanField(required=False, widget=forms.CheckboxInput())
 
-    def __init__(self, *args, **kwargs):
-        super(AsistenciaForm,self).__init__(*args,**kwargs)
+    def clean(self):
+        print('cleaneando')
+        return super(forms.Form,self).clean()
+
+    '''def __init__(self, *args, **kwargs):
+        super(AsistenciaForm,self).__init__(*args,**kwargs)'''
+
+AsistenciaFormset = formset_factory(AsistenciaForm)
--- clases/models.py
+++ clases/models.py
@@ -6,11 +6,14 @@ class Problema(models.Model):
     pass
 
 class Clase(models.Model):
-    nombre = models.CharField('nombre clase', max_length=100, unique=True, default="clase_default")
+    nombre = models.CharField('nombre clase', max_length=100, default="clase_default")
     problemas = models.ManyToManyField(Problema, related_name="profesoras", blank=True)
     publica = models.BooleanField('clase publica', default=True)
     fecha_clase = models.DateField("fecha clase", default=date.today)
     curso = models.ForeignKey(Curso, on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = (("nombre", "curso"))
 
     def __str__(self):
         return self.nombre
