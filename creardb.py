912d2fbbbaaa99738e568890dca86351b22b4df8
fermacias <fernanda.macias@ug.uchile.cl>
2020-01-29 13:08:20 -0300

    Bdd arreglada para que las alumans no se repitan en distintos cursos

--- creardb.py
+++ creardb.py
@@ -107,7 +107,6 @@ usuaria_alumna4 = User.objects.create_user(username="alumna4", first_name="Claud
 
 usuaria_alumna5 = User.objects.create_user(username="alumna5", first_name="Antonia", last_name="Quezada", password="contraseña123", es_alumna=True)
 
-## por agregar a la bdd
 usuaria_alumna6 = User.objects.create_user(username="alumna6", first_name="Fernanda", last_name="Macías", password="contraseña123", es_alumna=True)
 
 usuaria_alumna7 = User.objects.create_user(username="alumna7", first_name="Ignacia", last_name="Macías", password="contraseña123", es_alumna=True)
@@ -198,6 +197,140 @@ usuaria_alumna49 = User.objects.create_user(username="alumna49", first_name="Dom
 
 usuaria_alumna50 = User.objects.create_user(username="alumna50", first_name="Asunción", last_name="Gómez", password="contraseña123", es_alumna=True)
 
+## por agregar a la bdd
+
+usuaria_alumna51 = User.objects.create_user(username="alumna51", first_name="Fernanda", last_name="Macías", password="contraseña123", es_alumna=True)
+
+usuaria_alumna52 = User.objects.create_user(username="alumna52", first_name="Ignacia", last_name="Macías", password="contraseña123", es_alumna=True)
+
+usuaria_alumna53 = User.objects.create_user(username="alumna53", first_name="Ignacia", last_name="Labarca", password="contraseña123", es_alumna=True)
+
+usuaria_alumna54 = User.objects.create_user(username="alumna54", first_name="Karina", last_name="Rozas", password="contraseña123", es_alumna=True)
+
+usuaria_alumna55 = User.objects.create_user(username="alumna55", first_name="Florencia", last_name="Manríquez", password="contraseña123", es_alumna=True)
+
+usuaria_alumna56 = User.objects.create_user(username="alumna56", first_name="Antonia", last_name="Jimenez", password="contraseña123", es_alumna=True)
+
+usuaria_alumna57 = User.objects.create_user(username="alumna57", first_name="Constanza", last_name="Mora", password="contraseña123", es_alumna=True)
+
+usuaria_alumna58 = User.objects.create_user(username="alumna58", first_name="Rosario", last_name="González", password="contraseña123", es_alumna=True)
+
+usuaria_alumna59 = User.objects.create_user(username="alumna59", first_name="Lupe", last_name="Rojas", password="contraseña123", es_alumna=True)
+
+usuaria_alumna60 = User.objects.create_user(username="alumna60", first_name="Martina", last_name="Monsalves", password="contraseña123", es_alumna=True)
+
+usuaria_alumna61 = User.objects.create_user(username="alumna61", first_name="Belén", last_name="Acevedo", password="contraseña123", es_alumna=True)
+
+usuaria_alumna62 = User.objects.create_user(username="alumna62", first_name="Francisca", last_name="Riquelme", password="contraseña123", es_alumna=True)
+
+usuaria_alumna63 = User.objects.create_user(username="alumna63", first_name="Gabriela", last_name="Herrera", password="contraseña123", es_alumna=True)
+
+usuaria_alumna64 = User.objects.create_user(username="alumna64", first_name="Francisca", last_name="Fabres", password="contraseña123", es_alumna=True)
+
+usuaria_alumna65 = User.objects.create_user(username="alumna65", first_name="Bárbara", last_name="Fuentes", password="contraseña123", es_alumna=True)
+
+usuaria_alumna66 = User.objects.create_user(username="alumna66", first_name="Paula", last_name="Cifuentes", password="contraseña123", es_alumna=True)
+
+usuaria_alumna67 = User.objects.create_user(username="alumna67", first_name="Paulina", last_name="Lagos", password="contraseña123", es_alumna=True)
+
+usuaria_alumna68 = User.objects.create_user(username="alumna68", first_name="Alejandra", last_name="Rivas", password="contraseña123", es_alumna=True)
+
+usuaria_alumna69 = User.objects.create_user(username="alumna69", first_name="Mabel", last_name="Recabarren", password="contraseña123", es_alumna=True)
+
+usuaria_alumna70 = User.objects.create_user(username="alumna70", first_name="Teresa", last_name="Paredes", password="contraseña123", es_alumna=True)
+
+usuaria_alumna71 = User.objects.create_user(username="alumna71", first_name="Alicia", last_name="Cuadra", password="contraseña123", es_alumna=True)
+
+usuaria_alumna72 = User.objects.create_user(username="alumna72", first_name="Cecilia", last_name="Poblete", password="contraseña123", es_alumna=True)
+
+usuaria_alumna73 = User.objects.create_user(username="alumna73", first_name="Marta", last_name="Osorio", password="contraseña123", es_alumna=True)
+
+usuaria_alumna74 = User.objects.create_user(username="alumna74", first_name="Natalia", last_name="Arce", password="contraseña123", es_alumna=True)
+
+usuaria_alumna75 = User.objects.create_user(username="alumna75", first_name="Daniela", last_name="Medina", password="contraseña123", es_alumna=True)
+
+usuaria_alumna76 = User.objects.create_user(username="alumna76", first_name="Gabriela", last_name="Cuadra", password="contraseña123", es_alumna=True)
+
+usuaria_alumna77 = User.objects.create_user(username="alumna77", first_name="Luciana", last_name="Díaz", password="contraseña123", es_alumna=True)
+
+usuaria_alumna78 = User.objects.create_user(username="alumna78", first_name="Lucia", last_name="Cáceres", password="contraseña123", es_alumna=True)
+
+usuaria_alumna79 = User.objects.create_user(username="alumna79", first_name="Laura", last_name="Mierzo", password="contraseña123", es_alumna=True)
+
+usuaria_alumna80 = User.objects.create_user(username="alumna80", first_name="Scarlett", last_name="Suarez", password="contraseña123", es_alumna=True)
+
+usuaria_alumna81 = User.objects.create_user(username="alumna81", first_name="Catalina", last_name="Bravo", password="contraseña123", es_alumna=True)
+
+usuaria_alumna82 = User.objects.create_user(username="alumna82", first_name="Alejandra", last_name="Muñoz", password="contraseña123", es_alumna=True)
+
+usuaria_alumna83 = User.objects.create_user(username="alumna83", first_name="Josefa", last_name="Perez", password="contraseña123", es_alumna=True)
+
+usuaria_alumna84 = User.objects.create_user(username="alumna84", first_name="Josefina", last_name="Quiróz", password="contraseña123", es_alumna=True)
+
+usuaria_alumna85 = User.objects.create_user(username="alumna85", first_name="Monserrat", last_name="Martinez", password="contraseña123", es_alumna=True)
+
+usuaria_alumna86 = User.objects.create_user(username="alumna86", first_name="Valentina", last_name="Ruiz", password="contraseña123", es_alumna=True)
+
+usuaria_alumna87 = User.objects.create_user(username="alumna87", first_name="Margarita", last_name="Elfernan", password="contraseña123", es_alumna=True)
+
+usuaria_alumna88 = User.objects.create_user(username="alumna88", first_name="Javiera", last_name="Ramos", password="contraseña123", es_alumna=True)
+
+usuaria_alumna89 = User.objects.create_user(username="alumna89", first_name="María José", last_name="Del Campo", password="contraseña123", es_alumna=True)
+
+usuaria_alumna90 = User.objects.create_user(username="alumna90", first_name="Kiara", last_name="De la Maza", password="contraseña123", es_alumna=True)
+
+usuaria_alumna91 = User.objects.create_user(username="alumna91", first_name="Karen", last_name="James", password="contraseña123", es_alumna=True)
+
+usuaria_alumna92 = User.objects.create_user(username="alumna92", first_name="Sofía", last_name="Toledo", password="contraseña123", es_alumna=True)
+
+usuaria_alumna93 = User.objects.create_user(username="alumna93", first_name="Agustina", last_name="Timana", password="contraseña123", es_alumna=True)
+
+usuaria_alumna94 = User.objects.create_user(username="alumna94", first_name="Dominga", last_name="Espinoza", password="contraseña123", es_alumna=True)
+
+usuaria_alumna95 = User.objects.create_user(username="alumna95", first_name="Asunción", last_name="Gómez", password="contraseña123", es_alumna=True)
+
+usuaria_alumna96 = User.objects.create_user(username="alumna96", first_name="Karen", last_name="James", password="contraseña123", es_alumna=True)
+
+usuaria_alumna97 = User.objects.create_user(username="alumna97", first_name="Sofía", last_name="Toledo", password="contraseña123", es_alumna=True)
+
+usuaria_alumna98 = User.objects.create_user(username="alumna98", first_name="Agustina", last_name="Timana", password="contraseña123", es_alumna=True)
+
+usuaria_alumna99 = User.objects.create_user(username="alumna99", first_name="Dominga", last_name="Espinoza", password="contraseña123", es_alumna=True)
+
+usuaria_alumna100 = User.objects.create_user(username="alumna100", first_name="Asunción", last_name="Gómez", password="contraseña123", es_alumna=True)
+
+## 30 alumnas
+alumnas1 = [usuaria_alumna1, usuaria_alumna2, usuaria_alumna3, usuaria_alumna4, usuaria_alumna5,
+        usuaria_alumna6, usuaria_alumna7, usuaria_alumna8, usuaria_alumna9, usuaria_alumna10,
+        usuaria_alumna11, usuaria_alumna12, usuaria_alumna13, usuaria_alumna14, usuaria_alumna15,
+        usuaria_alumna16, usuaria_alumna17, usuaria_alumna18, usuaria_alumna19, usuaria_alumna20,
+        usuaria_alumna21, usuaria_alumna22, usuaria_alumna23, usuaria_alumna24, usuaria_alumna25,
+        usuaria_alumna26, usuaria_alumna27, usuaria_alumna28, usuaria_alumna29, usuaria_alumna30]
+
+## 30 alumnas
+alumnas2 = [usuaria_alumna31, usuaria_alumna32, usuaria_alumna33, usuaria_alumna34, usuaria_alumna35,
+        usuaria_alumna36, usuaria_alumna37, usuaria_alumna38, usuaria_alumna39, usuaria_alumna40,
+        usuaria_alumna41, usuaria_alumna42, usuaria_alumna43, usuaria_alumna44, usuaria_alumna45,
+        usuaria_alumna46, usuaria_alumna47, usuaria_alumna48, usuaria_alumna49, usuaria_alumna50,
+        usuaria_alumna51, usuaria_alumna52, usuaria_alumna53, usuaria_alumna54, usuaria_alumna55,
+        usuaria_alumna56, usuaria_alumna57, usuaria_alumna58, usuaria_alumna59, usuaria_alumna60]
+
+## 20 alumnas
+alumnas3 = [usuaria_alumna61, usuaria_alumna62, usuaria_alumna63, usuaria_alumna64, usuaria_alumna65,
+        usuaria_alumna66, usuaria_alumna67, usuaria_alumna68, usuaria_alumna69, usuaria_alumna70,
+        usuaria_alumna71, usuaria_alumna72, usuaria_alumna73, usuaria_alumna74, usuaria_alumna75,
+        usuaria_alumna76, usuaria_alumna77, usuaria_alumna78, usuaria_alumna79, usuaria_alumna80]
+
+## 5 alumnas
+alumnas4 = [usuaria_alumna81, usuaria_alumna82, usuaria_alumna83, usuaria_alumna84, usuaria_alumna85]
+
+## 5 alumnas
+alumnas5 = [usuaria_alumna86, usuaria_alumna87, usuaria_alumna88, usuaria_alumna89, usuaria_alumna90]
+
+## 5 alumnas
+alumnas6 = [usuaria_alumna91, usuaria_alumna92, usuaria_alumna93, usuaria_alumna94, usuaria_alumna95]
+
+
 
 """
 CASOS:
@@ -205,7 +338,8 @@ CASOS:
 2. CURSO SIN ALUMNAS
 3. CURSO SIN CLASES
 4. CURSO SIN ASISTENCIAS
-5. SIN ALUMNAS NI CLASES
+5. CURSO SIN ALUMNAS NI CLASES
+6. CURSO CON ASISTENCIAS, SIN PROX CLASE CREADA
 
 C++ básico FCFM - caso 1
 C++ medio FCFM - caso 2
@@ -219,92 +353,18 @@ C++ medio UMayor - caso 4
 C++ avanzado UMayor - caso 6
 """
 
-
 ## ALUMNAS INSCRITAS
-curso_basico.alumnas.add(usuaria_alumna1)
-curso_basico.alumnas.add(usuaria_alumna2)
-curso_basico.alumnas.add(usuaria_alumna3)
-curso_basico.alumnas.add(usuaria_alumna4)
-curso_basico.alumnas.add(usuaria_alumna5)
-curso_basico.alumnas.add(usuaria_alumna6)
-curso_basico.alumnas.add(usuaria_alumna7)
-curso_basico.alumnas.add(usuaria_alumna8)
-curso_basico.alumnas.add(usuaria_alumna9)
-curso_basico.alumnas.add(usuaria_alumna10)
-curso_basico.alumnas.add(usuaria_alumna11)
-curso_basico.alumnas.add(usuaria_alumna12)
-curso_basico.alumnas.add(usuaria_alumna13)
-curso_basico.alumnas.add(usuaria_alumna14)
-curso_basico.alumnas.add(usuaria_alumna15)
-curso_basico.alumnas.add(usuaria_alumna16)
-curso_basico.alumnas.add(usuaria_alumna17)
-curso_basico.alumnas.add(usuaria_alumna18)
-curso_basico.alumnas.add(usuaria_alumna19)
-curso_basico.alumnas.add(usuaria_alumna20)
-curso_basico.alumnas.add(usuaria_alumna21)
-curso_basico.alumnas.add(usuaria_alumna22)
-curso_basico.alumnas.add(usuaria_alumna23)
-curso_basico.alumnas.add(usuaria_alumna24)
-curso_basico.alumnas.add(usuaria_alumna25)
+
+def agregaAlumnas(alumnas, curso):
+    for alumna in alumnas:
+        curso.alumnas.add(alumna)
 
 
-curso_antofa.alumnas.add(usuaria_alumna26)
-curso_antofa.alumnas.add(usuaria_alumna27)
-curso_antofa.alumnas.add(usuaria_alumna28)
-curso_antofa.alumnas.add(usuaria_alumna29)
-curso_antofa.alumnas.add(usuaria_alumna30)
-
-
-curso_basico_umayor.alumnas.add(usuaria_alumna31)
-curso_basico_umayor.alumnas.add(usuaria_alumna32)
-curso_basico_umayor.alumnas.add(usuaria_alumna33)
-curso_basico_umayor.alumnas.add(usuaria_alumna34)
-curso_basico_umayor.alumnas.add(usuaria_alumna35)
-curso_basico_umayor.alumnas.add(usuaria_alumna36)
-curso_basico_umayor.alumnas.add(usuaria_alumna37)
-curso_basico_umayor.alumnas.add(usuaria_alumna38)
-curso_basico_umayor.alumnas.add(usuaria_alumna39)
-curso_basico_umayor.alumnas.add(usuaria_alumna40)
-curso_basico_umayor.alumnas.add(usuaria_alumna41)
-curso_basico_umayor.alumnas.add(usuaria_alumna42)
-curso_basico_umayor.alumnas.add(usuaria_alumna43)
-curso_basico_umayor.alumnas.add(usuaria_alumna44)
-curso_basico_umayor.alumnas.add(usuaria_alumna45)
-curso_basico_umayor.alumnas.add(usuaria_alumna46)
-curso_basico_umayor.alumnas.add(usuaria_alumna47)
-curso_basico_umayor.alumnas.add(usuaria_alumna48)
-curso_basico_umayor.alumnas.add(usuaria_alumna49)
-curso_basico_umayor.alumnas.add(usuaria_alumna50)
-
-
-curso_medio_umayor.alumnas.add(usuaria_alumna1)
-curso_medio_umayor.alumnas.add(usuaria_alumna2)
-curso_medio_umayor.alumnas.add(usuaria_alumna3)
-curso_medio_umayor.alumnas.add(usuaria_alumna4)
-curso_medio_umayor.alumnas.add(usuaria_alumna5)
-
-
-curso_avanzado_umayor.alumnas.add(usuaria_alumna6)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna7)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna8)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna9)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna10)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna11)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna12)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna13)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna14)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna15)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna16)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna17)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna18)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna19)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna20)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna21)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna22)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna23)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna24)
-curso_avanzado_umayor.alumnas.add(usuaria_alumna25)
-
+agregaAlumnas(alumnas1, curso_basico)
+agregaAlumnas(alumnas2, curso_basico_umayor)
+agregaAlumnas(alumnas3, curso_avanzado_umayor)
+agregaAlumnas(alumnas4, curso_antofa)
+agregaAlumnas(alumnas5, curso_medio_umayor)
 
 
 ## CLASES
@@ -390,29 +450,19 @@ clase_avanzado_umayor10 = Clase.objects.create(nombre="Clase 10: Problemas OCI 2
 
 ## ASISTENCIAS
 
-alumnas_basico = [usuaria_alumna1, usuaria_alumna2, usuaria_alumna3, usuaria_alumna4, usuaria_alumna5,
-                  usuaria_alumna6, usuaria_alumna7, usuaria_alumna8, usuaria_alumna9, usuaria_alumna10,
-                  usuaria_alumna11, usuaria_alumna12, usuaria_alumna13, usuaria_alumna14, usuaria_alumna15,
-                  usuaria_alumna16, usuaria_alumna17, usuaria_alumna18, usuaria_alumna19, usuaria_alumna20,
-                  usuaria_alumna21, usuaria_alumna22, usuaria_alumna23, usuaria_alumna24, usuaria_alumna25]
+alumnas_basico = alumnas1
 
 clases_basico = [clase_basico1, clase_basico2, clase_basico3, clase_basico4, clase_basico5,
                   clase_basico6, clase_basico7, clase_basico8, clase_basico9, clase_basico10,
                   clase_basico11, clase_basico12, clase_basico13]
 
-alumnas_basico_umayor = [usuaria_alumna31, usuaria_alumna32, usuaria_alumna33, usuaria_alumna34, usuaria_alumna35,
-                  usuaria_alumna36, usuaria_alumna37, usuaria_alumna38, usuaria_alumna39, usuaria_alumna40,
-                  usuaria_alumna41, usuaria_alumna42, usuaria_alumna43, usuaria_alumna44, usuaria_alumna45,
-                  usuaria_alumna46, usuaria_alumna47, usuaria_alumna48, usuaria_alumna49, usuaria_alumna50]
+alumnas_basico_umayor = alumnas2
 
 clases_basico_umayor = [clase_basico_umayor1, clase_basico_umayor2, clase_basico_umayor3, clase_basico_umayor4, clase_basico_umayor5,
                         clase_basico_umayor6, clase_basico_umayor7, clase_basico_umayor8, clase_basico_umayor9, clase_basico_umayor10,
                         clase_basico_umayor11, clase_basico_umayor12, clase_basico_umayor13]
 
-alumnas_avanzado_umayor = [usuaria_alumna6, usuaria_alumna7, usuaria_alumna8, usuaria_alumna9, usuaria_alumna10,
-                           usuaria_alumna11, usuaria_alumna12, usuaria_alumna13, usuaria_alumna14, usuaria_alumna15,
-                           usuaria_alumna16, usuaria_alumna17, usuaria_alumna18, usuaria_alumna19, usuaria_alumna20,
-                           usuaria_alumna21, usuaria_alumna22, usuaria_alumna23, usuaria_alumna24, usuaria_alumna25]
+alumnas_avanzado_umayor = alumnas3
 
 clases_avanzado_umayor = [clase_avanzado_umayor1, clase_avanzado_umayor2, clase_avanzado_umayor3, clase_avanzado_umayor4, clase_avanzado_umayor5,
                           clase_avanzado_umayor6, clase_avanzado_umayor7, clase_avanzado_umayor8, clase_avanzado_umayor9, clase_avanzado_umayor10]
@@ -423,8 +473,6 @@ def nuevaAsistencia(clases, alumnas, profesora):
     for clase in clases:
         for alumna in alumnas:
             Asistencia.objects.create(alumna=alumna, clase=clase, author=profesora, asistio=random.randint(0, 1))
-
-
 
 
 nuevaAsistencia(clases_basico, alumnas_basico, usuaria_profesora)
