48b873e698c5e15a938f003b6fabd37e56d148ae
antolabarca <anto.labarca.s@gmail.com>
2020-01-29 11:18:19 -0300

    - modificaciones para poder agregar imagenes (ya no crashea, pero tpco se ven)

--- NiñasProject/settings.py
+++ NiñasProject/settings.py
@@ -136,6 +136,10 @@ STATICFILES_FINDERS = (
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+
 #STATIC_ROOT = 'staticfiles'
 #STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATICFILES_DIRS = [
--- asistencia/urls.py
+++ asistencia/urls.py
@@ -1,10 +1,13 @@
-from django.urls import path, include
+from django.urls import path
 from .views import get_form
 from asistencia import views
+from django.conf import settings
+from django.conf.urls import url, include
+from django.conf.urls.static import static
 
 app_name = 'asistencia'
 
 urlpatterns = [
     path('<int:curso_id>/asistencia/', views.Asistencia_GralView.as_view(), name='asistencia_gral'),
     path('<int:curso_id>/asistencia/<int:clase_id>/', get_form, name='asistencia'),
-]
+] +static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
