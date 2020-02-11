3db9a2a9c3f01ebbf7e5cd687bb57c050fa2f5b0
kparga <karina.parga94@gmail.com>
2020-02-11 12:13:03 -0300

    sorry se me fue echarlos en el commit

--- staticfiles/js/scripts.js
+++ staticfiles/js/scripts.js
@@ -11,4 +11,37 @@ document.addEventListener('DOMContentLoaded', function() {
 
   });
 
+//modal asistencia de un curso (para una alumna)
 
+$(document).ready(function(){
+
+  $('.modal').modal();
+  
+$('#tabla_asist_gral').DataTable({
+    order:[[0, "asc"]],
+    dom: "Blfrtip",
+    oLanguage: {
+        sLengthMenu: '<span>Alumnas por página:</span><select class="browser-default">' +
+            '<option value="10">10</option>' +
+            '<option value="20">20</option>' +
+            '<option value="30">30</option>' +
+            '<option value="40">40</option>' +
+            '<option value="50">50</option>' +
+            '<option value="-1">Todas</option>' +
+            '</select></div>',
+        sSearch: "Buscar:",
+        sInfo: "Mostrando alumnas del _START_ al _END_ de un total de _TOTAL_ alumnas",
+        sInfoEmpty: "Mostrando registros del 0 al 0 de un total de 0 registros",
+        sInfoFiltered: "(filtrado de un total de _MAX_ alumnas)",
+        oPaginate: {
+                sFirst:    "Primero",
+                sLast:     "Último",
+                sNext:     "Siguiente",
+                sPrevious: "Anterior"
+            },
+        sZeroRecords: "No se encontraron resultados",
+    }
+    
+});
+$('select').material_select();
+});
