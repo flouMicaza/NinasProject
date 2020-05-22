4c3934322865efa6e67d56a777e3d7dff7505125
flourensia <flo.mirandai@gmail.com>
2020-05-21 23:15:01 -0400

    Admin de clases muestra nombre y curso de la clase

--- clases/admin.py
+++ clases/admin.py
@@ -1,4 +1,7 @@
 from django.contrib import admin
 from clases.models import Clase
 
-admin.site.register(Clase)
+@admin.register(Clase)
+class ClaseAdmin(admin.ModelAdmin):
+    base_model = Clase
+    list_display = ['__str__', 'curso']
