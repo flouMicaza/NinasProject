e922837e77262ef9803dce3b43b746527bc85d41
flourensia <flo.mirandai@gmail.com>
2020-05-21 23:07:49 -0400

    creacion del cambio de contrasena

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -6,5 +6,6 @@ app_name = 'usuarios'
 urlpatterns = [
     path('', views.IndexView.as_view(), name='index'),
     path('login/', views.LoginView.as_view(), name='login'),
-    path('logout/', views.LogoutView.as_view(), name='logout')
+    path('logout/', views.LogoutView.as_view(), name='logout'),
+    path('reset-password/', views.ResetPassword.as_view(),name='reset-password')
 ]
