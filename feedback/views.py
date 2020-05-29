bd304fe47367f255d7bd00402994860d0936cf2d
flourensia <flo.mirandai@gmail.com>
2020-05-29 15:02:17 -0400

    Se termina de procesar el form para agregar una sugerencia.

--- feedback/views.py
+++ feedback/views.py
@@ -7,7 +7,9 @@ from django.urls import reverse
 from django.utils.decorators import method_decorator
 from django.views import View
 
-from NiñasProject.decorators import  docente_required
+from NiñasProject.decorators import docente_required
+from clases.models import Clase
+from cursos.models import Curso
 from feedback.forms import OutputAlternativoModelFormSet
 from feedback.models import OutputAlternativo
 from problemas.models import Caso
@@ -33,7 +35,7 @@ class CasosAlternativos(LoginRequiredMixin, View):
             }
             return render(request, 'problemas/modal_casos_alternativos.html', context)
 
-        #TODO: si no es ajax que se debería hacer?
+        # TODO: si no es ajax que se debería hacer?
         else:
             print("la request no es ajax")
             return HttpResponseRedirect('/')
@@ -41,8 +43,23 @@ class CasosAlternativos(LoginRequiredMixin, View):
 
 @method_decorator([docente_required], name='dispatch')
 class ActualizarOutputsAlternativos(LoginRequiredMixin, View):
-    def post(self, request,**kwargs):
-        print("me llego el form!!!!!",kwargs['caso_id'])
-        return HttpResponseRedirect(reverse('problemas:casos-problema', kwargs={'curso_id':1, 'problema_id':52 , 'result':0}))
+    def post(self, request, **kwargs):
+        id_caso = kwargs['caso_id']
+        caso = Caso.objects.get(id=id_caso)
+        curso = self.get_curso(caso, request.user)
+        outputs_sugeridos = OutputAlternativo.objects.filter(caso=caso, agregado=False, frecuencia__gt=1)
+        formset = OutputAlternativoModelFormSet(request.POST, queryset=outputs_sugeridos)
+        if formset.is_valid():
+            instances = formset.save()
+            for inst in instances:
+                inst.agregado = True
+                inst.save()
+        return HttpResponseRedirect(
+            reverse('problemas:casos-problema', kwargs={'curso_id': curso.id, 'problema_id': caso.problema.id, 'result': 0}))
 
+    #FIX: esto asume que un problema solo pertenece a un curso!
+    def get_curso(self, caso, user):
+        clase = Clase.objects.filter(problemas__in=[caso.problema]) #clases en la que está este problema
+        curso = clase.values('curso') #cursos en los que está este problema
 
+        return Curso.objects.get(clase__in=clase)
