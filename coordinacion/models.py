9473a055925987363a1aee221f7f7524bf268fdc
isabelatc <isabela.tellechea@ug.uchile.cl>
2021-01-26 12:40:59 -0300

    Cambio en el modelo: sede contiene usuarias

--- coordinacion/models.py
+++ coordinacion/models.py
@@ -1,8 +1,14 @@
 from django.db import models
 
 # Create your models here.
+from usuarios.models import User
+
+
 class Sede(models.Model):
     nombre = models.CharField(max_length=100, default="Sede", primary_key=True)
+    profesoras = models.ManyToManyField(User, related_name="sedes_profesoras", blank=True)
+    voluntarias = models.ManyToManyField(User, related_name="sedes_voluntarias", blank=True)
+    alumnas = models.ManyToManyField(User, related_name="sedes_alumnas", blank=True)
 
     def __str__(self):
         return self.nombre
